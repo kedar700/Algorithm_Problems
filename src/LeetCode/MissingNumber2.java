3844e959953fd98172ba1d763871c2dcaea8a32a
Kedar Naik <knaik@fhlbdm.com>
2018-03-25 11:43:04 -0500

    Updated missing number 2

--- src/LeetCode/MissingNumber2.java
+++ src/LeetCode/MissingNumber2.java
@@ -7,13 +7,13 @@ public class MissingNumber2 {
 
     public static int[] missingNumber(int[] nums) {
         int sum = 0;
-        for(int num: nums)
+        for (int num : nums)
             sum += num;
 
 
-         int repeatedNumber = Math.abs(((nums.length * (nums.length + 1) ) - 2 * sum ) / 2);
-         int missingNumber = Math.abs(((nums.length * (nums.length + 1) ) - (2 * sum) - (2* repeatedNumber)  ) / 2);
-        int result[] = {repeatedNumber,missingNumber};
+        int repeatedNumber = Math.abs(((nums.length * (nums.length + 1)) - 2 * sum) / 2);
+        int missingNumber = Math.abs(((nums.length * (nums.length + 1)) - (2 * sum) - (2 * repeatedNumber)) / 2);
+        int result[] = {repeatedNumber, missingNumber};
         return result;
     }
 }
